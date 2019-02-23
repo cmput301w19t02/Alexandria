7af900e4ef55f1257915323a7cbe368722614a17
wowikc <bondarau@ualberta.c>
2019-02-23 13:15:56 -0700

    Update UserProfileTests.java

--- app/src/test/java/ca/ualberta/CMPUT3012019T02/alexandria/UserProfileTests.java
+++ app/src/test/java/ca/ualberta/CMPUT3012019T02/alexandria/UserProfileTests.java
@@ -41,4 +41,43 @@ public class UserProfileTests {
         userProfile.setPicture(picture);
         assertEquals(userProfile.getPicture(), picture);
     }
+
+    @Test
+    public void constructorTest(){
+        UserProfile userProfile = new UserProfile("John Smith","john@example.com","7801234567",null);
+        assertEquals(userProfile.getName(), "John Smith");
+        assertEquals(userProfile.getEmail(), "john@example.com");
+        assertEquals(userProfile.getPhone(), "7801234567");
+        assertEquals(userProfile.getPicture(), null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void nullNameConstructorTest() {
+        new UserProfile(null, "john@example.com", "7801234567", null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void emptyNameConstructorTest() {
+        new UserProfile("", "john@example.com", "7801234567", null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void nullPhoneConstructorTest() {
+        new UserProfile("John Smith", "john@example.com", null, null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void emptyPhoneConstructorTest() {
+        new UserProfile("John Smith", "john@example.com", "", null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void nullEmailConstructorTest() {
+        new UserProfile("John Smith", null, "7801234567", null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void emptyEmailConstructorTest() {
+        new UserProfile("John Smith", "", "7801234567", null);
+    }
 }
