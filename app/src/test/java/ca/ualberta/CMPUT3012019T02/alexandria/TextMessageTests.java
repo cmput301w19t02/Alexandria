376adfa7f3fd152dad352e5f123ebf324a929c79
sazzy4o <s.vonderohe40@gmail.com>
2019-02-23 16:13:51 -0700

    Fixed spaces and simplified

--- app/src/test/java/ca/ualberta/CMPUT3012019T02/alexandria/TextMessageTests.java
+++ app/src/test/java/ca/ualberta/CMPUT3012019T02/alexandria/TextMessageTests.java
@@ -4,71 +4,76 @@ import org.junit.Test;
 
 import java.util.Date;
 
-import ca.ualberta.CMPUT3012019T02.alexandria.model.Book;
+import ca.ualberta.CMPUT3012019T02.alexandria.model.message.TextMessage;
+
+import static org.junit.Assert.assertEquals;
 
 public class TextMessageTests {
     // positive constructor tests 
     @Test
     public void constructorTest() {
         Date date = new Date();
-        TextMessage message = new TextMessage("TEST content","unread", date, "johndoe@email.com");
-        Assert.assertEquals("TEST content", message.getContent());
-        Assert.assertEquals("unread", message.getStatus());
-        Assert.assertEquals(date, message.getDate());
-        Assert.assertEquals("johndoe@email.com", message.getSender());
-        Assert.assertEquals("text", message.getType());
+        TextMessage message = new TextMessage("TEST content", "unread", date, "johndoe@email.com");
+        assertEquals("TEST content", message.getContent());
+        assertEquals("unread", message.getStatus());
+        assertEquals(date, message.getDate());
+        assertEquals("johndoe@email.com", message.getSender());
+        assertEquals("text", message.getType());
     }
+
     // negative constructor tests
     @Test(expected = IllegalArgumentException.class)
     public void emptyTextConstructorTest() {
-        new TextMessage("", "unread", new Date(),"johndoe@email.com");
+        new TextMessage("", "unread", new Date(), "johndoe@email.com");
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void emptyStatusConstructorTest() {
-        new TextMessage("TEST content", "", new Date(),"johndoe@email.com");
+        new TextMessage("TEST content", "", new Date(), "johndoe@email.com");
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void emptySenderConstructorTest() {
-        new TextMessage("TEST content", "unread", new Date(),"");
+        new TextMessage("TEST content", "unread", new Date(), "");
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void nullTextConstructorTest() {
-        new TextMessage(null, "unread", new Date(),"johndoe@email.com");
+        new TextMessage(null, "unread", new Date(), "johndoe@email.com");
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void nullStatusConstructorTest(){
-        new TextMessage("TEST content", null, new Date(),"johndoe@email.com");
+    public void nullStatusConstructorTest() {
+        new TextMessage("TEST content", null, new Date(), "johndoe@email.com");
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void nullDateConstructorTest(){
-        new TextMessage("TEST content", "unread", null,"johndoe@email.com");
+    public void nullDateConstructorTest() {
+        new TextMessage("TEST content", "unread", null, "johndoe@email.com");
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void nullSenderConstructorTest(){
+    public void nullSenderConstructorTest() {
         new TextMessage("TEST content", "unread", new Date(), null);
     }
+
     // positive setText
     @Test
-    public void setTextTest(){
+    public void setTextTest() {
         TextMessage message = new TextMessage("TEST content", "unread", new Date(), "johndoe@email.com");
         message.setContent("Different content");
-        Assert.assertEquals("Different content", message.getText());
+        assertEquals("Different content", message.getText());
     }
+
     // negative setText
     @Test(expected = IllegalArgumentException.class)
-    public void emptySetTextTest(){
+    public void emptySetTextTest() {
         TextMessage message = new TextMessage("TEST content", "unread", new Date(), "johndoe@email.com");
         message.setText("");
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void nullSetTextTest(){
+    public void nullSetTextTest() {
         TextMessage message = new TextMessage("TEST content", "unread", new Date(), "johndoe@email.com");
         message.setText(null);
     }
