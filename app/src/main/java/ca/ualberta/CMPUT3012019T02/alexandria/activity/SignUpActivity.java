5708305d50df41cf3d2a887df296513b3bf476bb
sazzy4o <s.vonderohe40@gmail.com>
2019-02-23 11:27:29 -0700

    Add signup and login activity skeletons

--- app/src/main/java/ca/ualberta/CMPUT3012019T02/alexandria/activity/SignUpActivity.java
+++ app/src/main/java/ca/ualberta/CMPUT3012019T02/alexandria/activity/SignUpActivity.java
@@ -6,38 +6,41 @@ import android.view.View;
 
 import ca.ualberta.CMPUT3012019T02.alexandria.R;
 
-/**
- * A login screen that offers login via email/password.
- */
-public class LoginActivity extends AppCompatActivity {
-
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sign_up);
     }
 
-    public void signIn(View view) {
+    public void addPhoto(View view) {
         //TODO Implement
         throw new UnsupportedOperationException("Not implemented");
     }
 
-    public void navigateToSignUp(View view) {
+    public void signUp(View view) {
         //TODO Implement
         throw new UnsupportedOperationException("Not implemented");
     }
 
+    public void showOrHidePassword(View view) {
+        //TODO Implement
+        throw new UnsupportedOperationException("Not implemented");
+    }
 
     private boolean validateUsername(String username) {
         //TODO: Replace this with better logic
         return true;
     }
 
+    private boolean validateEmail(String email) {
+        //TODO: Replace this with better logic
+        return email.contains("@") && email.contains(".");
+    }
+
     private boolean validatePassword(String password) {
         //TODO: Replace this with better logic
         return password.length() >= 8;
     }
-
 }
-
