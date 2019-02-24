d03abfdd6b51519b302f307b7e733ead609e4598
tdpr2 <tdr@ualberta.ca>
2019-02-23 22:22:54 -0700

    reverted changes to LocationMessage

--- app/src/main/java/ca/ualberta/CMPUT3012019T02/alexandria/model/message/LocationMessage.java
+++ app/src/main/java/ca/ualberta/CMPUT3012019T02/alexandria/model/message/LocationMessage.java
@@ -7,21 +7,16 @@ import ca.ualberta.CMPUT3012019T02.alexandria.model.Location;
 public class LocationMessage extends Message {
 
     public LocationMessage(Location content, String status, Date date, String sender) {
-        if (!(content instanceof Location.class)) {
-            throw new IllegalArgumentException("location content needs to be a Location object");
-        }
-
         super("location", content, status, date, sender);
     }
 
     public Location getLocation() {
-        return this.content;
+        // TODO Finish implementation
+        throw new UnsupportedOperationException();
     }
 
     public void setLocation(Location location) {
-        if (!(content instanceof Location.class)) {
-            throw new IllegalArgumentException("location content needs to be a Location object");
-        }
-        this.content = location;
+        // TODO Finish implementation
+        throw new UnsupportedOperationException();
     }
 }
