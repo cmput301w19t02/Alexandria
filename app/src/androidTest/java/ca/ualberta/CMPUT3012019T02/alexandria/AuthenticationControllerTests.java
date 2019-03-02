c0a827ef976a3d1084affd3f5161aeaa9b477f00
sazzy4o <s.vonderohe40@gmail.com>
2019-03-02 16:35:14 -0700

    Implement auth controller and tests

---
+++ app/src/androidTest/java/ca/ualberta/CMPUT3012019T02/alexandria/AuthenticationControllerTests.java
@@ -0,0 +1,125 @@
+package ca.ualberta.CMPUT3012019T02.alexandria;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+import ca.ualberta.CMPUT3012019T02.alexandria.activity.MainActivity;
+import ca.ualberta.CMPUT3012019T02.alexandria.controller.AuthenticationController;
+import java9.util.concurrent.CompletableFuture;
+
+/**
+ * This class tests the AuthenticationController
+ */
+@RunWith(AndroidJUnit4.class)
+public class AuthenticationControllerTests {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testCreateUser() throws InterruptedException, ExecutionException, TimeoutException {
+        AuthenticationController controller = AuthenticationController.getInstance();
+
+        String username = "test_"+UUID.randomUUID().toString().replace('-', '_');
+        String password = UUID.randomUUID().toString();
+        String email = username + "@example.com";
+        CompletableFuture<Void> future = controller.createUser(username, email, password);
+
+        future.get(5, TimeUnit.SECONDS);
+
+        Assert.assertTrue(controller.isAuthenticated());
+    }
+
+    @Test(expected = ExecutionException.class)
+    public void testCreateUserUsernameAlreadyTaken() throws InterruptedException, ExecutionException, TimeoutException {
+        AuthenticationController controller = AuthenticationController.getInstance();
+
+        String username = "0457de6b_0a85_481a_9093_c73de1ba0020";
+        String password = UUID.randomUUID().toString();
+        String email = username + "@example.com";
+        CompletableFuture<Void> future = controller.createUser(username, email, password);
+
+        future.get(5, TimeUnit.SECONDS);
+    }
+
+
+    @Test(expected = ExecutionException.class)
+    public void testCreateUserEmailExists() throws InterruptedException, ExecutionException, TimeoutException {
+        AuthenticationController controller = AuthenticationController.getInstance();
+
+        String username = "test_"+UUID.randomUUID().toString().replace('-', '_');
+        String password = UUID.randomUUID().toString();
+        String email = "0457de6b_0a85_481a_9093_c73de1ba0020@example.com";
+        CompletableFuture<Void> future = controller.createUser(username, email, password);
+
+        future.get(5, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testAuthenticate() throws InterruptedException, ExecutionException, TimeoutException {
+        AuthenticationController controller = AuthenticationController.getInstance();
+
+        String username = "0457de6b_0a85_481a_9093_c73de1ba0020";
+        String password = "4b5e9592-8c9e-4c37-b7d6-f5aed797e791";
+        String email = username + "@example.com";
+        CompletableFuture<Void> future = controller.authenticate(username, password);
+
+        future.get(5, TimeUnit.SECONDS);
+
+        Assert.assertTrue(controller.isAuthenticated());
+    }
+
+    @Test(expected = ExecutionException.class)
+    public void testAuthenticateBadCredentials() throws InterruptedException, ExecutionException, TimeoutException {
+        AuthenticationController controller = AuthenticationController.getInstance();
+
+        String username = "0457de6b_0a85_481a_9093_c73de1ba0020";
+        String password = "bad password";
+        String email = username + "@example.com";
+        CompletableFuture<Void> future = controller.authenticate(username, password);
+
+        future.get(5, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testDeauthenticate() throws InterruptedException, ExecutionException, TimeoutException {
+        AuthenticationController controller = AuthenticationController.getInstance();
+
+        String username = "0457de6b_0a85_481a_9093_c73de1ba0020";
+        String password = "4b5e9592-8c9e-4c37-b7d6-f5aed797e791";
+        String email = username + "@example.com";
+        CompletableFuture<Void> future = controller.authenticate(username, password);
+
+        future.get(5, TimeUnit.SECONDS);
+
+        Assert.assertTrue(controller.isAuthenticated());
+
+        controller.deauthenticate();
+
+        Assert.assertFalse(controller.isAuthenticated());
+    }
+
+    @Test
+    public void testGetMyId() throws InterruptedException, ExecutionException, TimeoutException {
+        AuthenticationController controller = AuthenticationController.getInstance();
+
+        String username = "0457de6b_0a85_481a_9093_c73de1ba0020";
+        String password = "4b5e9592-8c9e-4c37-b7d6-f5aed797e791";
+        String email = username + "@example.com";
+        CompletableFuture<Void> future = controller.authenticate(username, password);
+
+        future.get(5, TimeUnit.SECONDS);
+
+        Assert.assertTrue(controller.isAuthenticated());
+        Assert.assertEquals("eQgZfhN2Yng9TPHcXvfBZs5ZKxj1",controller.getMyId());
+    }
+}
--- app/src/main/java/ca/ualberta/CMPUT3012019T02/alexandria/controller/AuthenticationController.java
+++ app/src/main/java/ca/ualberta/CMPUT3012019T02/alexandria/controller/AuthenticationController.java
@@ -1,14 +1,34 @@
 package ca.ualberta.CMPUT3012019T02.alexandria.controller;
 
+import android.support.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java9.util.concurrent.CompletableFuture;
+
 
 public class AuthenticationController {
     private FirebaseAuth auth;
+    private FirebaseDatabase database;
 
     private static AuthenticationController instance;
 
-    private AuthenticationController(){ }
+    private AuthenticationController(){
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+    }
 
+    /**
+     * Gets the singleton instance of AuthenticationController
+     * @return the AuthenticationController instance
+     */
     public static AuthenticationController getInstance() {
         if (instance == null) {
             instance = new AuthenticationController();
@@ -16,21 +36,115 @@ public class AuthenticationController {
         return instance;
     }
 
-    public boolean authenticate(String username, String password) {
-        // TODO Implement
-        throw new UnsupportedOperationException("Not implemented");
+    /**
+     * Determines and returns if a user is authenticated
+     * @return if the user is authenticated
+     */
+    public boolean isAuthenticated() {
+        return auth.getCurrentUser() != null;
     }
 
+    /**
+     * Logs the current user in
+     * @param username the user's username
+     * @param password the user's password
+     * @return a CompletableFuture signifying this operation's success/failure
+     */
+    public CompletableFuture<Void> authenticate(String username, final String password) {
+        final CompletableFuture<Void> resultFuture = new CompletableFuture<>();
+        database.getReference().child("usernameToEmail").child(username).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    auth.signInWithEmailAndPassword((String) dataSnapshot.getValue(),password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                resultFuture.complete(null);
+                            }
+                            else{
+                                resultFuture.completeExceptionally(task.getException());
+                            }
+                        }
+                    });
+                }
+                else{
+                    resultFuture.completeExceptionally(new IllegalArgumentException("Username does not exist"));
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                resultFuture.completeExceptionally(databaseError.toException());
+            }
+        });
+        return resultFuture;
+    }
+
+    /**
+     * Signs the current user out
+     */
     public void deauthenticate() {
-        // TODO Implement
-        throw new UnsupportedOperationException("Not implemented");
+        auth.signOut();
     }
 
-    public boolean createUser(String username, String password) {
-        // TODO Implement
-        throw new UnsupportedOperationException("Not implemented");
+    /**
+     * Creates a new user account
+     * @param username the user's new username
+     * @param email the user's email
+     * @param password the user's password
+     * @return a CompletableFuture signifying this operation's success/failure
+     */
+    public CompletableFuture<Void> createUser(final String username, final String email, final String password) {
+        final CompletableFuture<Void> resultFuture = new CompletableFuture<>();
+
+            database.getReference().child("usernameToEmail").child(username).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if(!dataSnapshot.exists()){
+                        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> authTask) {
+                                if(authTask.isSuccessful()){
+                                    database.getReference().child("usernameToEmail").child(username).setValue(email)
+                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<Void> databaseTask) {
+                                                    if(databaseTask.isSuccessful()){
+                                                        resultFuture.complete(null);
+                                                    }
+                                                    else{
+                                                        database.getReference().child("usernameToEmail").child(username).removeValue();
+                                                        resultFuture.completeExceptionally(databaseTask.getException());
+                                                    }
+                                                }
+                                            });
+                                }
+                                else {
+                                    database.getReference().child("usernameToEmail").child(username).removeValue();
+                                    resultFuture.completeExceptionally(authTask.getException());
+                                }
+                            }
+                        });
+                    }
+                    else {
+                        resultFuture.completeExceptionally(new IllegalArgumentException("Username already exists"));
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    resultFuture.completeExceptionally(databaseError.toException());
+                }
+            });
+
+        return resultFuture;
     }
 
+    /**
+     * Gets and return the current user's id
+     * @return the current user's id
+     */
     public String getMyId() {
         return auth.getUid();
     }
