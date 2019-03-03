6b536f705390e0a3f090619d69d1c031eea7669d
wowikc <bondarau@ualberta.c>
2019-03-02 22:08:10 -0700

    blocked users setup

--- app/src/main/java/ca/ualberta/CMPUT3012019T02/alexandria/activity/BlockedUsersActivity.java
+++ app/src/main/java/ca/ualberta/CMPUT3012019T02/alexandria/activity/BlockedUsersActivity.java
@@ -2,6 +2,8 @@ package ca.ualberta.CMPUT3012019T02.alexandria.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import ca.ualberta.CMPUT3012019T02.alexandria.R;
 
@@ -11,5 +13,18 @@ public class BlockedUsersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blocked_users);
+
+        // toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.blocked_users_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);    // remove default title
+
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_yellow_24dp);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
