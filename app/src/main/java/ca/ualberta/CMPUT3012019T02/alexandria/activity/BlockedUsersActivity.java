47318bcb4c89a3f403909b9d1fe12078cf403637
wowikc <bondarau@ualberta.c>
2019-03-11 01:31:33 -0600

    remove test button

--- app/src/main/java/ca/ualberta/CMPUT3012019T02/alexandria/activity/BlockedUsersActivity.java
+++ app/src/main/java/ca/ualberta/CMPUT3012019T02/alexandria/activity/BlockedUsersActivity.java
@@ -28,10 +28,4 @@ public class BlockedUsersActivity extends AppCompatActivity {
             }
         });
     }
-
-    //TODO remove when there is a straight pass to view user
-    protected void onJoeTestClick(View view) {
-        Intent startProfileActivity = new Intent(this, ViewUserProfileActivity.class);
-        startActivity(startProfileActivity);
-    }
 }
