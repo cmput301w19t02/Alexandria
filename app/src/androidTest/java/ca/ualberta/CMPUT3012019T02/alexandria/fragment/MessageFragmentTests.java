ca2172a6054f52d7c20f1ed164714e4f7ea02959
TyRobinson <ty.robinson@trustscience.com>
2019-03-13 23:07:00 -0600

    setting up framework for intent test

--- app/src/androidTest/java/ca/ualberta/CMPUT3012019T02/alexandria/fragment/MessageFragmentTests.java
+++ app/src/androidTest/java/ca/ualberta/CMPUT3012019T02/alexandria/fragment/MessageFragmentTests.java
@@ -1,8 +1,38 @@
 package ca.ualberta.CMPUT3012019T02.alexandria.fragment;
 
+import android.app.Activity;
+import android.support.v4.app.Fragment;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+import ca.ualberta.CMPUT3012019T02.alexandria.R;
+import ca.ualberta.CMPUT3012019T02.alexandria.activity.MainActivity;
+
 /**
  * This class tests the MessageFragment
  */
+@RunWith(AndroidJUnit4.class)
 public class MessageFragmentTests {
-    // Intent test MessageFragment
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule = new
+            ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void init(){
+        Fragment fragment = new MessagesFragment();
+        activityTestRule.getActivity().getSupportFragmentManager()
+                .beginTransaction().replace(R.id.fragment_container, fragment).commit();
+
+    }
+
+    @Test
+    public void testChatRoomIntent(){
+         onView(withId(R.id.));
+    }
 }
