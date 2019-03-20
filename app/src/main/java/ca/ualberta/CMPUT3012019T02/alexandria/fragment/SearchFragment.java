9b23a06456b58bb33b099d37cda63e567c9d226a
Otman <35116578+omaoued@users.noreply.github.com>
2019-03-20 07:34:38 -0600

    Edit SearchFragment UI behavior

--- app/src/main/java/ca/ualberta/CMPUT3012019T02/alexandria/fragment/SearchFragment.java
+++ app/src/main/java/ca/ualberta/CMPUT3012019T02/alexandria/fragment/SearchFragment.java
@@ -9,14 +9,17 @@ import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
@@ -57,10 +60,28 @@ public class SearchFragment extends Fragment {
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         mRecyclerView.setAdapter(bookAdapter);
 
-        searchText = rootView.findViewById(R.id.search_input);
-        searchText.requestFocus();
         InputMethodManager imgr = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
         imgr.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
+
+        // toolbar
+        Toolbar toolbar = rootView.findViewById(R.id.toolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        // remove default title
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back);
+        toolbar.setNavigationOnClickListener((View v) -> {
+            imgr.hideSoftInputFromWindow(rootView.getWindowToken(), 0);
+            getFragmentManager().popBackStack();
+        });
+
+        searchText = rootView.findViewById(R.id.search_input);
+        searchText.requestFocus();
+
+        //sets the scroll behaviour for screen, removes navigation bar from bottom
+        //absolutely no easy way for me to do this, spent a good time trying to get an alternative
+        getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+        getActivity().findViewById(R.id.navigation).setVisibility(View.GONE);
 
         searchText.addTextChangedListener(new TextWatcher() {
 
