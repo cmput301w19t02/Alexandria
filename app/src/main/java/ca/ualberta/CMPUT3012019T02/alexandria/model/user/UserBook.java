3be2548c24a568b581ebe5776f47430e79b71a0c
Deric Cheung <cheung.deric@gmail.com>
2019-03-25 13:49:50 -0600

    Redo javadoc comments and annotations

--- app/src/main/java/ca/ualberta/CMPUT3012019T02/alexandria/model/user/UserBook.java
+++ app/src/main/java/ca/ualberta/CMPUT3012019T02/alexandria/model/user/UserBook.java
@@ -45,12 +45,11 @@ public abstract class UserBook {
     }
 
     /**
-     * Sets the status of this user book
-     * TO BE USED ONLY BY SUBCLASSES AND BOOK CONTROLLER
+     * Locally sets the status of this user book. Does not affect the database in any way.
+     * All status changes happen through the {@link ca.ualberta.CMPUT3012019T02.alexandria.controller.BookController}
      *
      * @param status status to set
      */
-    @Deprecated
     public void setStatus(String status) {
         if (status.equals("available") || status.equals("requested") || status.equals("accepted") || status.equals("borrowed")) {
             this.status = status;
@@ -71,18 +70,21 @@ public abstract class UserBook {
     /**
      * Whether or not this book has been scanned
      *
-     * @return true if this book has been scanned. false otherwise
+     * @return True if this book has been scanned. False otherwise
      */
     public boolean getScanned() {
         return scanned;
     }
 
     /**
-     * Sets 'scanned' status of this book to true
-     * TO BE USED ONLY BY SUBCLASSES AND BOOK CONTROLLER
+     * Locally sets 'scanned' status of this book to true. Does not affect the database in any way.
+     * Use {@link ca.ualberta.CMPUT3012019T02.alexandria.controller.BookController#scanMyBorrowedBook(String)}
+     * and {@link ca.ualberta.CMPUT3012019T02.alexandria.controller.BookController#scanMyOwnedBook(String)}
+     * to scan books.
+     *
+     * @param b True if this book should be scanned. False otherwise.
      */
     @Exclude
-    @Deprecated
     public void setScanned(boolean b) {
         if (b && (status.equals("accepted") || status.equals("borrowed"))) {
             scanned = true;
